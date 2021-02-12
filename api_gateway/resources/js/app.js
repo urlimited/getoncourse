692777fe5a1c7241485dd476c4120390530223e5
zhandos <sad@awe>
2021-02-13 00:33:10 +0600

    Initial commit - just test

---
+++ api_gateway/resources/js/app.js
@@ -0,0 +1,1 @@
+console.log("adwawda");
--- deployment/scripts/components/nodejs.sh
+++ deployment/scripts/components/nodejs.sh
@@ -2,6 +2,8 @@
 sudo apt-get update
 sudo apt-get install nodejs -y
 sudo apt-get install npm -y
+
+cd ./api_gateway
 sudo npm install -g npm
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
