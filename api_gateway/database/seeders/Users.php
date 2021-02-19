03e06ea9491435b54f4ef6e795fb2c00b0521912
maximtsoy <maxim.tsoy.cv@gmail.com>
2021-02-19 14:55:46 +0600

    Added seeder and roles for api gateway

---
+++ api_gateway/database/seeders/Users.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserRole;
+use Illuminate\Database\Seeder;
+
+class Users extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app('db')->table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin'.'@test.com',
+            'password' => app('hash')->make('localadmin'),
+            'role' => UserRole::USER_ROLE_ADMIN
+        ]);
+
+        app('db')->table('users')->insert([
+            'name' => 'teacher',
+            'email' => 'teacher'.'@test.com',
+            'password' => app('hash')->make('localteacher'),
+            'role' => UserRole::USER_ROLE_TEACHER
+        ]);
+    }
+}
