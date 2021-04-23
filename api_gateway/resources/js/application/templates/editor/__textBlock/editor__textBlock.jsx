ab4b22d03b7e1b24b774d1a9d8d2508b22af3197
maximtsoy <maxim.tsoy.cv@gmail.com>
2021-04-23 09:12:59 +0600

    - now command is identified with regular expression, and command ends with space

--- api_gateway/resources/js/application/templates/editor/__textBlock/editor__textBlock.jsx
+++ api_gateway/resources/js/application/templates/editor/__textBlock/editor__textBlock.jsx
@@ -5,6 +5,26 @@ const getCaretCoordinates = require('textarea-caret');
 const Editor__textBlock = ({id, deleteHandler, setDropdownCommandsConfigsHandler}) => {
 
     const [content, setContent] = useState('');
+    const [caretPosition, setCaretPosition] = useState({});
+
+    const [command, setCommand] = useState('');
+    const [isCommandStarted, setIsCommandStarted] = useState(false);
+
+    useEffect(() => {
+        if(isCommandStarted)
+            setDropdownCommandsConfigsHandler({
+                position: caretPosition,
+                isVisible: true
+            })
+        else
+            setDropdownCommandsConfigsHandler({
+                isVisible: false
+            })
+    }, [isCommandStarted]);
+
+    useEffect(() => {
+        console.log(command);
+    }, [command]);
 
     return (<div>
         <textarea placeholder={"Type '/' for commands"}
@@ -13,19 +33,29 @@ const Editor__textBlock = ({id, deleteHandler, setDropdownCommandsConfigsHandler
                           deleteHandler(id);
 
                       setContent(e.target.value)
+
+                      if(isCommandStarted){
+                          const regex = `.{${caretPosition.charStarted}}\\/(.*).{${(e.target.value.length - e.target.selectionStart)}}`;
+
+                          if((new RegExp(regex, "gm")).exec(e.target.value))
+                              setCommand((new RegExp(regex, "gm")).exec(e.target.value)[1]);
+                      }
                   }}
                   onKeyPress={e => {
                       if (e.code === "Slash"){
                           const caret = getCaretCoordinates(e.target, e.target.selectionEnd);
 
-                          setDropdownCommandsConfigsHandler({
-                              position: {
-                                  x: e.target.offsetLeft + caret.left,
-                                  y: e.target.offsetTop + caret.top
-                              },
-                              isVisible: true
+                          setCaretPosition({
+                              x: e.target.offsetLeft + caret.left,
+                              y: e.target.offsetTop + caret.top,
+                              charStarted: e.target.selectionEnd
                           });
+
+                          setIsCommandStarted(true);
                       }
+
+                      if (e.code === 'Space')
+                          setIsCommandStarted(false);
 
                   }}
                   onBlur={e => setDropdownCommandsConfigsHandler({
