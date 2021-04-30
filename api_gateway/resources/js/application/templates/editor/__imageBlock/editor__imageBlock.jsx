ba4e93ec89bbec78496d53e55b41800efae889b2
maximtsoy <maxim.tsoy.cv@gmail.com>
2021-05-01 03:05:13 +0600

    - added styles to images block

--- api_gateway/resources/js/application/templates/editor/__imageBlock/editor__imageBlock.jsx
+++ api_gateway/resources/js/application/templates/editor/__imageBlock/editor__imageBlock.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef, useState} from 'react';
 import "./require.css";
 
-const Editor__imageBlock = ({deleteBlockHandler, setHtmlElementHandler}) => {
+const Editor__imageBlock = ({deleteBlockHandler, setHtmlElementHandler, id}) => {
     const [imageSrc, setImageSrc] = useState('');
     const [file, setFile] = useState();
 
@@ -16,7 +16,9 @@ const Editor__imageBlock = ({deleteBlockHandler, setHtmlElementHandler}) => {
     return (<div className="editor__image-block">
         <div className="editor__image-block-input">
             <input type="file"
+                   style={{display: 'none'}}
                    ref={selfHtmlElement}
+                   id={"input-file-" + id}
                    onChange={e => {
                        const reader = new FileReader();
 
@@ -30,15 +32,24 @@ const Editor__imageBlock = ({deleteBlockHandler, setHtmlElementHandler}) => {
         <div className="editor__image-block-preview"
              onMouseEnter={() => setIsImageHovered(true)}
              onMouseLeave={() => setIsImageHovered(false)}>
-            <img src={imageSrc} alt="The image" className="editor__image-block-preview-image"/>
+            {
+                file
+                    ? <img src={imageSrc} alt="The image" className="editor__image-block-preview-image"/>
+                    : <div className="editor__image-placeholder">
+                        {isImageHovered
+                            ? ""
+                            : "Выберите изображение"
+                        }</div>
+            }
             {isImageHovered
                 ? <div className="editor__image-block-preview-actions">
-                    <button
-                        onClick={e => deleteBlockHandler()}>Delete
+                    <button className="editor__image-block-preview-actions-delete"
+                            onClick={e => deleteBlockHandler()}>
+                        Del
                     </button>
-                    <button
-                        onClick={e => console.log('changed')}>Change
-                    </button>
+                    <label htmlFor={"input-file-" + id} className="editor__image-block-preview-actions-update"
+                           onClick={e => console.log('changed')}>Upd
+                    </label>
                 </div>
                 : <></>}
         </div>
--- api_gateway/resources/js/application/templates/editor/models/editorImageBlock.model.tsx
+++ api_gateway/resources/js/application/templates/editor/models/editorImageBlock.model.tsx
@@ -24,6 +24,7 @@ export class EditorImageBlockModel extends EditorBlockModelFile.EditorBlockModel
     public render(key: number): React.ReactElement {
         return <ImageBlock
             key={this._key}
+            id={this._key}
             setHtmlElementHandler={(htmlElement: HTMLElement) => this.setHtmlElement(htmlElement)}
             deleteBlockHandler={() => this._handlers.deleteBlockHandler(this)}
         />
